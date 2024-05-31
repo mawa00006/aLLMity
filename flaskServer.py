340596e44cce2cc51d77166a5cb6ad822a5bbd97
Mattes <mattes.warning@gmx.de>
2024-05-31 08:29:33 +0200

    Initial commit

---
+++ flaskServer.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, redirect, url_for, request
+import random
+import json
+import csv
+
+from static.utils import *
+
+# Create a Flask web application instance
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+# Define the root route, which redirects to the 'base' route
+@app.route('/')
+def index():
+    return redirect(url_for('base'))
+
+
+# Define the 'base' route, which renders the 'base.html' template
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+
+@app.route('/generate_letter', methods=['GET'])
+def generate_letter():
+    patient_id = request.args.get('patient_id')
+    letter_type = request.args.get('type')
+
+    print(letter_type)
+    print(patient_id)
+
+    arztbriefe, pflegedokumentation = get_data(patient_id, letter_type)
+
+    letter = write_arztbrief(arztbriefe, pflegedokumentation)
+
+
+    # Logic to generate the doctor's letter based on patient_id and letter_type
+    letter_content = letter
+
+    return letter_content
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ static/utils.py
@@ -0,0 +1,64 @@
+import codecs
+import os
+import google.generativeai as genai
+GOOGLE_API_KEY = "AIzaSyDCWi33umYkREk9hxYFJqafYkaA42qxPZk"
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
+
+def get_data(patient_id, letter_type):
+
+    arztbriefe = ""
+    pflegedokumentation = ""
+
+    arztbriefe = read_arztbrief(patient_id)
+
+    if letter_type == 1:
+        pflegedokumentation += read_pflegedokumentation(patient_id)
+
+    return arztbriefe, pflegedokumentation
+
+
+def read_document2(filepath):
+    with open(filepath, 'r') as file:
+        data = file.read().replace('\n', '')
+
+    return data
+
+def read_document(filepath):
+    with codecs.open(filepath, 'r', encoding='utf-8') as file:
+
+        data = ""
+        for line in file:
+            line = line.replace('\n', '').replace('\r', '')
+            data += line
+
+    return data
+
+def read_arztbrief(patient_id):
+    path = f"data/{patient_id}_arzt.txt"
+    arztbriefe = read_document(path)
+    return arztbriefe
+
+def read_pflegedokumentation(patient_id):
+    path = f"data/{patient_id}_pflege.txt"
+    pflegedokumentation = read_document(path)
+    return pflegedokumentation
+
+def write_arztbrief(arzttexte, pflegetexte):
+    persona = "Du bist ein Stationsarzt in einem Krankenhaus. Du sitzt in deinem Büro am Computer "
+    task = ("und du schreibst einen fachlichen Arztbrief auschließlich aus den folgenden Daten. Konzentriere dich dabei"
+            " auschließlich auf relevante ereignisse: ")
+    context = " Arzttexte: " + arzttexte + " Pflegetexte: " + pflegetexte
+
+    format = "Format: gebe nicht diese zewichen aus: \n* , \n"
+
+    prompt = persona + task + context + format
+
+    # LLM befragen
+    response = model.generate_content(prompt)
+    print(response)
+
+    arztbrief = str(response._result.candidates[0].content.parts[0]).replace('\n', '')
+
+
+    return arztbrief
